c1b1219ecaf1e29dc5d0441bfcd68c7dc8141b4e
Etourneau Gwenn <gwenn.etourneau@gmail.com>
2016-07-06 14:09:38 +0900

    Adding staging information

--- script/stage.sh
+++ script/stage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+root=$PWD
 export CF_STACK=cflinuxfs2
-cp -Rr app-source/. /home/vcap/app/
+cp -Rr $root/app-source/. /home/vcap/app/
 export PATH=$PATH:/home/vcap/app/php/bin
-cd stage && ../buildpack/bin/compile /home/vcap/app /tmp/cache
+cd /stage && ../buildpack/bin/compile /home/vcap/app /tmp/cache
 
